b1b096a559b54d136e85171881b3b562a3ae168c
DESKTOP-JPBOFE2\badda <baddam.548@gmail.com>
2020-11-23 23:02:18 +0000

    code

---
+++ Arrays/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    public class Class1
+    {
+        public int[] i = { 10, 20, 30 };
+        public double[] d = { 123, 236, 369 };
+        public string[] s = { "naveen", "rajesh", "mahesh" };
+        public float[] f = { 12.36f, 36.23f, 25.21f };
+    }
+}
